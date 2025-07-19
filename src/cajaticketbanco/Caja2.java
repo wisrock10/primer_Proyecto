7fa7611cc9314402267fdb4768186dfc85fc139b
User <User@LAPTOP-P6L223C6.lan>
2025-07-19 17:54:07 -0600

    git commit -m "setimo commit: Se creo la clase Caja2 y se le agrego las variables"

---
+++ src/cajaticketbanco/Caja2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cajaticketbanco;
+
+/**
+ *@author Wiston Rivas
+ * @author  Cristian Rojas
+ */
+public class Caja2 {
+    private int cantidadAtendidos = 0;
+    private int tiempoTotal = 0; // Total time in minutes
+}
