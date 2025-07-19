fa612d390eba1dddda096cd52edecea98f7b4692
qadri <qadri@Wiston>
2025-07-19 14:30:13 -0600

    git commit -m "Quinto commit: Creando la clase caja 1 y definiendola"

---
+++ src/cajaticketbanco/Caja1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cajaticketbanco;
+import java.util.List;
+import java.util.Random;
+/**
+ *
+ * @author qadri
+ */
+
+//Caja 1 atiende a todos los clientes excepto los que requieren atención en plataforma.
+public class Caja1 {
+    private int cantidadAtendidos = 0;
+    private int tiempoTotal = 0; // Total time in minutes
+
+    // Attends each ticket by simulating random time
+    public void atenderTickets(List<String> tickets) {
+        System.out.println("Iniciando atención en Caja 1...");
+        Random random = new Random();
+
+        for (String ticket : tickets) {
+            char letter = ticket.charAt(0);
+            String number = ticket.substring(1);
+
+            System.out.println("Caja 1 atiende ticket número " + number + " con letra " + letter);
+
+            int minutos = 1 + random.nextInt(5);  // Random minutes between 1 and 5
+            System.out.println("Tiempo estimado de atención: " + minutos + " minutos");
+
+            cantidadAtendidos++;
+            tiempoTotal += minutos;
+        }
+
+        System.out.println("Atención finalizada en Caja 1.");
+    }
+}
