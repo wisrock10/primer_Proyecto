2d479c6382919424afb91dad25f01394dd2ebd04
User <User@LAPTOP-P6L223C6.lan>
2025-07-19 18:20:00 -0600

    git commit -m "doceabo commit: agregamos metodos se agrego los imports y se arreglaron las llaves"

--- src/cajaticketbanco/Caja2.java
+++ src/cajaticketbanco/Caja2.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cajaticketbanco;
-
+import java.util.List;
+import java.util.Random;
 /**
  *@author Wiston Rivas
  * @author  Cristian Rojas
@@ -11,27 +12,34 @@ package cajaticketbanco;
 public class Caja2 {
     private int cantidadAtendidos = 0;
     private int tiempoTotal = 0; // Total time in minutes
-}
-// Attends each ticket by simulating random time
+
+    // Attends each ticket by simulating random time
     public void atenderTickets(List<String> tickets) {
         System.out.println("Iniciando atención en Caja 2...");
         Random random = new Random();
 
-for (String ticket : tickets) {
+        for (String ticket : tickets) {
             char letter = ticket.charAt(0);
             String number = ticket.substring(1);
 
-System.out.println("Caja 2 atiende ticket número " + number + " con letra " + letter);
+            System.out.println("Caja 2 atiende ticket número " + number + " con letra " + letter);
 
             int minutos = 1 + random.nextInt(5);  // Random minutes between 1 and 5
             System.out.println("Tiempo estimado de atención: " + minutos + " minutos");
 
             cantidadAtendidos++;
             tiempoTotal += minutos;
-}
+        }
 
         System.out.println("Atención finalizada en Caja 2.");
     }
 
     public int getCantidadAtendidos() {
         return cantidadAtendidos;
+    }
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+}
+
